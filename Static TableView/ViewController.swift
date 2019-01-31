73a3212b1c8cd87f7dc7eb063054a3cf5e44d36a
R. Kukuh <rkukuh@gmail.com>
2019-01-31 09:49:29 +0700

    Initial Commit

---
+++ Static TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Static TableView
+//
+//  Created by R. Kukuh on 31/01/19.
+//  Copyright © 2019 R. Kukuh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
